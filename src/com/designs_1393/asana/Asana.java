ea37a0ea5db1195509fc0443b8e73fa9f3f3d38c
Sean Barag <sjbarag@gmail.com>
2012-05-13 08:59:18 -0400

    Initial commit.
    Seriously, this isn't even a "Hello, World!"

---
+++ src/com/designs_1393/asana/Asana.java
@@ -0,0 +1,15 @@
+package com.designs_1393.asana;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Asana extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

08e533829a055b8e26e0a33476b61847a7ed5415
Sean Barag <sjbarag@gmail.com>
2012-05-26 08:59:17 -0400

    Remove old API key dialog.
    This will likely be replaced with a dialog fragment, but it probably won't be a
    drop-in replacement.

--- src/com/designs_1393/asana/Asana.java
+++ src/com/designs_1393/asana/Asana.java
@@ -12,47 +12,7 @@ import android.os.Bundle;
  */
 public class Asana extends SherlockFragmentActivity
 {
-	final String TAG = "Asana";
-	private SharedPreferences sharedPrefs;
-	private Context ctx;
-
-	private Cursor workspaceCursor;
-
-	/*private void getUserApiKey()
-	{
-		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder( getApplicationContext() );
-		LayoutInflater inflater = getLayoutInflater();
-		View dialoglayout = inflater.inflate(R.layout.apikeydialog,
-			(ViewGroup) getCurrentFocus());
-		dialogBuilder.setView( dialoglayout );
-
-		final EditText apiKeyInput = (EditText)
-			dialoglayout.findViewById(R.id.apiKeyEntry);
-		final TextView apiInstructions = (TextView)
-			dialoglayout.findViewById(R.id.apiKeyInstructions);
-
-		Linkify.addLinks( apiInstructions, Linkify.WEB_URLS );
-
-		dialogBuilder.setCancelable( true );
-		dialogBuilder.setPositiveButton( "Okay", new DialogInterface.OnClickListener() {
-			public void onClick( DialogInterface dialog, int which )
-			{
-				if( sharedPrefs == null )
-					Log.i(TAG, "Shared prefs is null");
-				SharedPreferences.Editor editor = sharedPrefs.edit();
-				if( editor == null )
-					Log.i(TAG, "Editor is null");
-				editor.putString("api key", apiKeyInput.getText().toString());
-				editor.commit();
-			}
-		});
-
-		dialogBuilder.setNegativeButton( "Cancel", new DialogInterface.OnClickListener() {
-			public void onClick( DialogInterface dialog, int which ){}
-		});
-
-		dialogBuilder.create().show();
-	}*/
+	final String APP_TAG = "Asana";
 
 	/** Called when the activity is first created. */
 	@Override

0563b2169bab092d973f03ef7234c51fd0479b60
Sean Barag <sjbarag@gmail.com>
2012-05-27 11:59:38 -0400

    Prevent NullPointerException from getView().
    However, there are still issues.
        Caused by: android.util.AndroidRuntimeException: requestFeature() must be
        called before adding content
    I'm not sure what this applies to.

--- src/com/designs_1393/asana/ApiKeyDialogFragment.java
+++ src/com/designs_1393/asana/ApiKeyDialogFragment.java
@@ -32,7 +32,7 @@ import com.actionbarsherlock.app.SherlockDialogFragment;
 
 public class ApiKeyDialogFragment extends SherlockDialogFragment
 {
-	private final String APP_TAG = "ApiKeyDialogFragment";
+	private final String APP_TAG = "Asana.ApiKeyDialogFragment";
 	private static SharedPreferences sharedPrefs;
 
 	public static ApiKeyDialogFragment newInstance( SharedPreferences prefs )
@@ -48,39 +48,32 @@ public class ApiKeyDialogFragment extends SherlockDialogFragment
 	                          ViewGroup      container,
 	                          Bundle         savedInstanceState )
 	{
+		Log.i( APP_TAG, "in onCreateView" );
 		View v = inflater.inflate(R.layout.apikeydialog, container );
 		TextView tv = (TextView) v.findViewById( R.id.apiKeyInstructions );
 
 		// apply links
 		Linkify.addLinks( tv, Linkify.WEB_URLS );
 
+		if( v == null )
+			Log.e( APP_TAG, "OH JEEZ v IS NULL" );
+
+		Log.i( APP_TAG, "leaving onCreateView" );
 		return v;
 	}
 
 	@Override
 	public Dialog onCreateDialog( Bundle savedInstanceState )
 	{
-		View layout = getView();
-		final EditText apiKeyInput =
-			(EditText) layout.findViewById( R.id.apiKeyEntry );
+		Log.i( APP_TAG, "in onCreateDialog()" );
 
 		return new AlertDialog.Builder( getActivity() )
 			.setTitle( "API Key" )
-			.setView( layout )
 			.setCancelable( true )
 			.setPositiveButton( "Okay", new DialogInterface.OnClickListener() {
 					public void onClick( DialogInterface dialog, int which )
 					{
-						SharedPreferences.Editor editor =
-							ApiKeyDialogFragment.sharedPrefs.edit();
-						if( editor == null )
-						{
-							Log.i(APP_TAG, "Editor is null");
-						}
-						editor.putString(
-							"api key",
-							apiKeyInput.getText().toString());
-						editor.commit();
+						((Asana)getActivity()).doApiPositiveClick();
 					}
 				})
 			.setNegativeButton( "Cancel", new DialogInterface.OnClickListener() {
--- src/com/designs_1393/asana/Asana.java
+++ src/com/designs_1393/asana/Asana.java
@@ -1,9 +1,26 @@
 package com.designs_1393.asana;
 
+import com.designs_1393.asana.workspace.*;
+
+// General
+import android.os.Bundle;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.Context;
+
+// UI
+import android.view.View;
+import android.widget.EditText;
+
+// Fragments
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+
 // ActionBarSherlock
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.app.SherlockDialogFragment;
 
-import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Main application activity.
@@ -13,12 +30,46 @@ import android.os.Bundle;
 public class Asana extends SherlockFragmentActivity
 {
 	final String APP_TAG = "Asana";
+	private SharedPreferences sharedPrefs;
+	private SherlockDialogFragment apiDialog;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
+
+		sharedPrefs = getSharedPreferences("AsanaPrefs", Context.MODE_PRIVATE);
+
+		if(sharedPrefs.getString( "api key", "not found" ).equals("not found"))
+		{
+			showApiKeyDialog();
+		}
+
 		setContentView( R.layout.workspace_list );
+	}
+
+	private void showApiKeyDialog()
+	{
+		// create and show the dialog
+		apiDialog = ApiKeyDialogFragment.newInstance(sharedPrefs);
+		apiDialog.show( getSupportFragmentManager(), "dialog" );
+	}
+
+	public void doApiPositiveClick()
+	{
+		View v = apiDialog.getView();
+		EditText apiKeyInput =
+			(EditText) v.findViewById(R.id.apiKeyInstructions);
+
+		SharedPreferences.Editor editor = sharedPrefs.edit();
+		if( editor == null )
+		{
+			Log.i(APP_TAG, "Editor is null");
+		}
+		editor.putString(
+			"api key",
+			apiKeyInput.getText().toString());
+		editor.commit();
 	}
 }

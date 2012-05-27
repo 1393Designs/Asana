a6859be5ae1a96758158c836f58d9bc410c168e2
Sean Barag <sjbarag@gmail.com>
2012-05-27 09:27:45 -0400

    Nearly complete implementation of Project class.
    Still requires a User and UserSet class.

--- src/com/designs_1393/asana/Project/Project.java
+++ src/com/designs_1393/asana/Project/Project.java
@@ -6,6 +6,7 @@ import com.designs_1393.asana.workspace.*;
 
 public class Project
 {
+	private long      ID;
 	private boolean   archived;
 	private String    createdAt; // TODO: this should be parsed to a real time
 	//  private UserSet   followers; TODO: UserSet does not exist yet.
@@ -35,7 +36,27 @@ public class Project
 		modifiedAt = "";
 		name       = "";
 		notes      = "";
-		workspace  = "";
+		workspace  = new Workspace();
+	}
+
+	/**
+	 * Returns the project's unique identifier from Asana.
+	 * @return unique identieifer for this worpsace, represented as a long int.
+	 */
+	public long getID()
+	{
+		return ID;
+	}
+
+	/**
+	 * Sets the project's unique identifier, as provided by Asana.
+	 * This exists mainly to facilitate the use of the Jackson JSON API.
+	 * @param projectID  Unique long int identifier for the proejct, as
+	 *                   provided by Asana.
+	 */
+	public void setID( long projectID )
+	{
+		ID = projectID;
 	}
 
 	/**
@@ -78,24 +99,104 @@ public class Project
 		createdAt = time;
 	}
 
+//	/**
+//	 * Returns the set of users following this project.
+//	 * @return a UserSet containing the list of users following this project.
+//	 */
+//	public UserSet getFollowers()
+//	{
+//		return followers;
+//	}
+//
+//	/**
+//	 * Sets the list of users following this project.
+//	 * Because this is a read-only property within Asana's API, this method
+//	 * exists only to facilitate the use of the Jackson JSON API.
+//	 * @param users  a UserSet containing the list of users following this
+//	 *               project.
+//	 */
+//	public void setFollowers( UserSet users )
+//	{
+//		followers = users;
+//	}
+
 	/**
-	 * Returns the set of users following this project.
-	 * @return a UserSet containing the list of users following this project.
+	 * Returns the time at which this project was last modified on Asana's
+	 * servers.
+	 * @return a String representation of a time, e.g. 2012-02-22T02:06:58.147Z.
 	 */
-	public UserSet getFollowers()
+	public String getModifiedAt()
 	{
-		return followers;
+		return modifiedAt;
 	}
 
 	/**
-	 * Sets the list of users following this project.
+	 * Sets the time at which this project was last modified on Asana's servers.
 	 * Because this is a read-only property within Asana's API, this method
 	 * exists only to facilitate the use of the Jackson JSON API.
-	 * @param users  a UserSet containing the list of users following this
-	 *               project.
+	 * @param time  a String representation of a time, e.g.
+	 *              2012-02-22T02:06:58.147Z.
 	 */
-	public void setFollowers( UserSet users )
+	public void setModifiedAt( String time )
 	{
-		followers = users;
+		modifiedAt = time;
+	}
+
+	/**
+	 * Returns the name of the project.
+	 * @return a String containing the name of the project.
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * Sets the name of this project.
+	 * @param text  a String containing the name of the project.
+	 */
+	public void setName( String text )
+	{
+		name = text;
+	}
+
+	/**
+	 * Returns the notes stored with the project.
+	 * @return a String containing the notes stored with the project.
+	 */
+	public String getNotes()
+	{
+		return notes;
+	}
+
+	/**
+	 * Sets the notes stored with the project.
+	 * @param text  a String containing the notes stored with the project.
+	 */
+	public void setNotes( String text )
+	{
+		notes = text;
+	}
+
+	/**
+	 * Returns the workspace this project is associated with.
+	 * @return a Workspace object containing the ID and name of the workspace
+	 *         this project is associated with.
+	 */
+	public Workspace getWorkspace()
+	{
+		return workspace;
+	}
+
+	/**
+	 * Sets the workspace this project is associated with.
+	 * Note that once created, projects cannot be moved to a different
+	 * workspace.
+	 * @param ws  a Workspace object containing the ID and name of the
+	 *            workspace this project is associated with.
+	 */
+	public void setWorkspace( Workspace ws )
+	{
+		workspace = ws;
 	}
 }

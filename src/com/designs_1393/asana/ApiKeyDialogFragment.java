aa9bd728924b30ad0620ebf50bde58979d5807a9
Sean Barag <sjbarag@gmail.com>
2012-05-28 08:18:02 -0400

    Properly show dialog to prompt user for API Key
    The "requestFeature()" FC from the past few commits was caused by using
    DialogFragment.onCreateView() to inflate the view.  Instead, it should be
    inflated in onCreateDialog() and set using an AlertDialog.Builder with
    .setView() as the FIRST builder operation (or at least before .setTitle() or
    anything else that will change the UI).

--- src/com/designs_1393/asana/ApiKeyDialogFragment.java
+++ src/com/designs_1393/asana/ApiKeyDialogFragment.java
@@ -44,36 +44,38 @@ public class ApiKeyDialogFragment extends SherlockDialogFragment
 	}
 
 	@Override
-	public View onCreateView( LayoutInflater inflater,
-	                          ViewGroup      container,
-	                          Bundle         savedInstanceState )
-	{
-		Log.i( APP_TAG, "in onCreateView" );
-		View v = inflater.inflate(R.layout.apikeydialog, container );
-		TextView tv = (TextView) v.findViewById( R.id.apiKeyInstructions );
-
-		// apply links
-		Linkify.addLinks( tv, Linkify.WEB_URLS );
-
-		if( v == null )
-			Log.e( APP_TAG, "OH JEEZ v IS NULL" );
-
-		Log.i( APP_TAG, "leaving onCreateView" );
-		return v;
-	}
-
-	@Override
 	public Dialog onCreateDialog( Bundle savedInstanceState )
 	{
 		Log.i( APP_TAG, "in onCreateDialog()" );
 
+		View v = getActivity()
+		         .getLayoutInflater()
+		         .inflate(R.layout.apikeydialog, null);
+		TextView instructions =
+			(TextView) v.findViewById( R.id.apiKeyInstructions );
+
+		final EditText apiKeyInput =
+			(EditText) v.findViewById( R.id.apiKeyEntry );
+
+		// apply links to instructions
+		Linkify.addLinks( instructions, Linkify.WEB_URLS );
+
 		return new AlertDialog.Builder( getActivity() )
+			.setView( v )
 			.setTitle( "API Key" )
 			.setCancelable( true )
 			.setPositiveButton( "Okay", new DialogInterface.OnClickListener() {
 					public void onClick( DialogInterface dialog, int which )
 					{
-						((Asana)getActivity()).doApiPositiveClick();
+						SharedPreferences.Editor editor = sharedPrefs.edit();
+						if( editor == null )
+						{
+							Log.i(APP_TAG, "Editor is null");
+						}
+						editor.putString(
+							"api key",
+							apiKeyInput.getText().toString());
+						editor.commit();
 					}
 				})
 			.setNegativeButton( "Cancel", new DialogInterface.OnClickListener() {
--- src/com/designs_1393/asana/Asana.java
+++ src/com/designs_1393/asana/Asana.java
@@ -5,22 +5,13 @@ import com.designs_1393.asana.workspace.*;
 // General
 import android.os.Bundle;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.content.Context;
 
-// UI
-import android.view.View;
-import android.widget.EditText;
-
 // Fragments
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.DialogFragment;
 
 // ActionBarSherlock
 import com.actionbarsherlock.app.SherlockFragmentActivity;
-import com.actionbarsherlock.app.SherlockDialogFragment;
-
-import android.util.Log;
 
 /**
  * Main application activity.
@@ -31,7 +22,7 @@ public class Asana extends SherlockFragmentActivity
 {
 	final String APP_TAG = "Asana";
 	private SharedPreferences sharedPrefs;
-	private SherlockDialogFragment apiDialog;
+	private DialogFragment apiKeyDialog;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -43,33 +34,11 @@ public class Asana extends SherlockFragmentActivity
 
 		if(sharedPrefs.getString( "api key", "not found" ).equals("not found"))
 		{
-			showApiKeyDialog();
+			// create and show the dialog
+			apiKeyDialog = ApiKeyDialogFragment.newInstance(sharedPrefs);
+			apiKeyDialog.show( getSupportFragmentManager(), "dialog" );
 		}
 
 		setContentView( R.layout.workspace_list );
-	}
-
-	private void showApiKeyDialog()
-	{
-		// create and show the dialog
-		apiDialog = ApiKeyDialogFragment.newInstance(sharedPrefs);
-		apiDialog.show( getSupportFragmentManager(), "dialog" );
-	}
-
-	public void doApiPositiveClick()
-	{
-		View v = apiDialog.getView();
-		EditText apiKeyInput =
-			(EditText) v.findViewById(R.id.apiKeyInstructions);
-
-		SharedPreferences.Editor editor = sharedPrefs.edit();
-		if( editor == null )
-		{
-			Log.i(APP_TAG, "Editor is null");
-		}
-		editor.putString(
-			"api key",
-			apiKeyInput.getText().toString());
-		editor.commit();
 	}
 }
